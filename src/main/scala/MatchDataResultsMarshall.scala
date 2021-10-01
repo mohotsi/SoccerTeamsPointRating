ff299e5e8ce8a3b41458f26d70eb5f44ededdde4
thapelo.mohotsi <thapelo.mohotsi@interfront.co.za>
2021-10-01 19:55:07 +0200

    create scala jar

--- src/main/scala/MatchDataResultsMarshall.scala
+++ src/main/scala/MatchDataResultsMarshall.scala
@@ -26,11 +26,7 @@ def getGameMatches()={
     }.groupBy(_.teamName).map(teamScore => TeamPoints(teamScore._1, teamScore._2.map(_.points).sum)).toList.sortBy(_.points).reverse
   }
 
-  def PointsEarnedPerTeam():List[TeamPoints]={
-    getTeamNames().flatMap { team =>
-      getGameMatches().map(game => TeamPoints(team, getPointsEarnedInGameMatch(team, game)))
-    }.groupBy(_.teamName).map(teamScore => TeamPoints(teamScore._1, teamScore._2.map(_.points).sum)).toList
-  }
+
 
   def getPointsEarnedInGameMatch(teamName: String, game: Game): Int = {
     if (game.teams.indexWhere(_.teamName == teamName) == -1) {
