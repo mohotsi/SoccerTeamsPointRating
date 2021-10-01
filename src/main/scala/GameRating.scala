7c93313ec4213af2a84e90f28807b8a9fb373eda
thapelo.mohotsi <thapelo.mohotsi@interfront.co.za>
2021-10-01 15:12:12 +0200

    clean up the code

--- src/main/scala/GameRating.scala
+++ src/main/scala/GameRating.scala
@@ -1,48 +1,54 @@
 import scala.io.Source
-case class Score(teamName:String, goals:Int)
-case class Match(team: List[Score])
-case class TeamPoints(teamName:String, points:Int)
+
+case class Score(teamName: String, goals: Int)
+
+case class Game(team: List[Score])
+case class Team(name: String)
+case class TeamPoints(teamName: String, points: Int)
 object GameRating extends App {
-  case class Team(name:String)
+  val data = Source.fromFile("./src/main/scala/matchesResults.txt").getLines().toList
 
-val data=Source.fromFile("./src/main/scala/matchesResults.txt").getLines().toList
+  // get all the team name list
+  val teamNames = data.flatMap(game => game.split(",")).map(_.replaceAll("([a-zA-Z\\s]+)\\s\\d+", "$1"))
+    .distinct
 
-val teams=data.flatMap(game=>game.split(",")).map(_.replaceAll("([a-zA-Z\\s]+)\\s\\d+","$1"))
-  .distinct
+  val games = data.map(game => game.split(",").toList.map { side =>
+    def take(variable: String) = side.replaceAll("([a-zA-Z\\s]+)\\s(\\d)+", variable)
 
-val matches=data.map(game=>game.split(",").toList.map{side=>
-  def take(variable:String)=side.replaceAll("([a-zA-Z\\s]+)\\s(\\d)+",variable)
-  Score(take("$1"),take("$2").toInt)
-}).map(Match)
-val teamsPoints=teams.flatMap { team =>
-  matches.map(game => TeamPoints(team,getGamePoint(team,game)))
-}.groupBy(_.teamName).map(teamScore=>TeamPoints(teamScore._1,teamScore._2.map(_.points).sum)).toList.sortBy(_.points).reverse
+    Score(take("$1"), take("$2").toInt)
+  }).map(Game)
 
-  (0 to teamsPoints.length-1).foreach { index =>
-    val team =teamsPoints(index)
-      println(s"""${index + 1}. ${team.teamName.trim}, ${team.points} pts""")
+  val pointsEarnedPerTeam = teamNames.flatMap { team =>
+    games.map(game => TeamPoints(team, getGameEarnedPoints(team, game)))
+  }.groupBy(_.teamName).map(teamScore => TeamPoints(teamScore._1, teamScore._2.map(_.points).sum)).toList.sortBy(_.points).reverse
+
+
+
+  (0 to pointsEarnedPerTeam.length - 1).foreach { index =>
+    val team = pointsEarnedPerTeam(index)
+    println(s"""${index + 1}. ${team.teamName.trim}, ${team.points} pts""")
   }
 
 
-def  getGamePoint(teamName:String, game:Match): Int = {
-  if (game.team.indexWhere(_.teamName == teamName) == -1) {
-    0
-  }
-  else {
-    val teamsScores = game.team.partition(_.teamName == teamName)
-    val teamScore = teamsScores._1.head.goals
-    val opponentScore = teamsScores._2.head.goals
-    if (teamScore == opponentScore) {
-      1
-    }
-    else if (teamScore > opponentScore) {
-      3
-    }
-    else { //teamScore < opponentScore
+
+  def getGameEarnedPoints(teamName: String, game: Game): Int = {
+    if (game.team.indexWhere(_.teamName == teamName) == -1) {
       0
     }
+    else {
+      val teamsScores = game.team.partition(_.teamName == teamName)
+      val teamScore = teamsScores._1.head.goals
+      val opponentScore = teamsScores._2.head.goals
+      if (teamScore == opponentScore) {
+        1
+      }
+      else if (teamScore > opponentScore) {
+        3
+      }
+      else { //teamScore < opponentScore
+        0
+      }
 
+    }
   }
-}
-
 }
