e2f380ebf018c1eda08b1ee7e3d95b9288a94545
thapelo.mohotsi <thapelo.mohotsi@interfront.co.za>
2021-10-01 16:51:51 +0200

    Re structuring code so that it is easy to test

--- src/main/scala/GameRating.scala
+++ src/main/scala/GameRating.scala
@@ -1,26 +1,10 @@
-import scala.io.Source
 
-case class Score(teamName: String, goals: Int)
 
-case class Game(team: List[Score])
-case class Team(name: String)
-case class TeamPoints(teamName: String, points: Int)
 object GameRating extends App {
-  val data = Source.fromFile("./src/main/scala/matchesResults.txt").getLines().toList
+  val data = new MatchResultsMarshall("./src/main/scala/matchesResults.txt")
 
-  // get all the team name list
-  val teamNames = data.flatMap(game => game.split(",")).map(_.replaceAll("([a-zA-Z\\s]+)\\s\\d+", "$1"))
-    .distinct
 
-  val games = data.map(game => game.split(",").toList.map { side =>
-    def take(variable: String) = side.replaceAll("([a-zA-Z\\s]+)\\s(\\d)+", variable)
-
-    Score(take("$1"), take("$2").toInt)
-  }).map(Game)
-
-  val pointsEarnedPerTeam = teamNames.flatMap { team =>
-    games.map(game => TeamPoints(team, getGameEarnedPoints(team, game)))
-  }.groupBy(_.teamName).map(teamScore => TeamPoints(teamScore._1, teamScore._2.map(_.points).sum)).toList.sortBy(_.points).reverse
+  val pointsEarnedPerTeam =data.getPointsEarnedPerMatch()
 
 
 
@@ -31,24 +15,4 @@ object GameRating extends App {
 
 
 
-  def getGameEarnedPoints(teamName: String, game: Game): Int = {
-    if (game.team.indexWhere(_.teamName == teamName) == -1) {
-      0
-    }
-    else {
-      val teamsScores = game.team.partition(_.teamName == teamName)
-      val teamScore = teamsScores._1.head.goals
-      val opponentScore = teamsScores._2.head.goals
-      if (teamScore == opponentScore) {
-        1
-      }
-      else if (teamScore > opponentScore) {
-        3
-      }
-      else { //teamScore < opponentScore
-        0
-      }
-
-    }
-  }
 }
---
+++ src/main/scala/TextLIstReadExtent.scala
@@ -0,0 +1,55 @@
+
+
+import scala.io.Source
+
+case class Score(teamName: String, goals: Int)
+
+case class Game(teams: List[Score])
+case class Team(name: String)
+case class TeamPoints(teamName: String, points: Int)
+class MatchResultsMarshall(fileUrl:String) {
+  val data = Source.fromFile("./src/main/scala/matchesResults.txt").getLines().toList
+def getTeamNames()={
+  data.flatMap(game => game.split(",")).map(_.replaceAll("([a-zA-Z\\s]+)\\s\\d+", "$1"))
+    .distinct
+}
+def getGameMatches()={
+  data.map(game => game.split(",").toList.map { side =>
+    def take(variable: String) = side.replaceAll("([a-zA-Z\\s]+)\\s(\\d)+", variable)
+
+    Score(take("$1"), take("$2").toInt)
+  }).map(Game)
+}
+  def getPointsEarnedPerMatch()={
+    getTeamNames().flatMap { team =>
+      getGameMatches().map(game => TeamPoints(team, getPointsEarnedInGameMatch(team, game)))
+    }.groupBy(_.teamName).map(teamScore => TeamPoints(teamScore._1, teamScore._2.map(_.points).sum)).toList.sortBy(_.points).reverse
+  }
+
+  def PointsEarnedPerTeam()={
+    getTeamNames().flatMap { team =>
+      getGameMatches().map(game => TeamPoints(team, getPointsEarnedInGameMatch(team, game)))
+    }.groupBy(_.teamName).map(teamScore => TeamPoints(teamScore._1, teamScore._2.map(_.points).sum)).toList.sortBy(_.points).reverse
+  }
+
+  def getPointsEarnedInGameMatch(teamName: String, game: Game): Int = {
+    if (game.teams.indexWhere(_.teamName == teamName) == -1) {
+      0
+    }
+    else {
+      val teamsScores = game.teams.partition(_.teamName == teamName)
+      val teamScore = teamsScores._1.head.goals
+      val opponentScore = teamsScores._2.head.goals
+      if (teamScore == opponentScore) {
+        1
+      }
+      else if (teamScore > opponentScore) {
+        3
+      }
+      else { //teamScore < opponentScore
+        0
+      }
+
+    }
+  }
+}
